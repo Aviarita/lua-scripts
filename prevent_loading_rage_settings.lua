997999ea82189f8701ecd7081d2f92266b20a12b
Aviarita <43707639+Aviarita@users.noreply.github.com>
2018-09-29 20:11:50 +0200

    Create prevent_loading_rage_settings.lua

---
+++ prevent_loading_rage_settings.lua
@@ -0,0 +1,31 @@
+local SetUi = ui.set
+local GetUi = ui.get
+local aimbot = ui.reference("RAGE", "Aimbot", "Enabled")
+local spread = ui.reference("RAGE", "Other", "Remove spread")
+local recoil = ui.reference("RAGE", "Other", "Remove recoil")
+local pitch = ui.reference("AA", "Anti-Aimbot angles", "Pitch")
+local yaw = ui.reference("AA", "Anti-Aimbot angles", "Yaw")
+local fakeyaw = ui.reference("AA", "Anti-Aimbot angles", "Fake yaw")
+local anti_ut = ui.reference("MISC", "Settings", "Anti-untrusted")
+
+local checkbox = ui.new_checkbox("MISC", "Other", "Prevent loading rage settings")
+
+local GetAll = entity.get_all
+local GetProp = entity.get_prop
+
+local AddEvent = client.set_event_callback
+
+AddEvent("paint", function()
+    local CCSGameRulesProxy = GetAll("CCSGameRulesProxy")[1]
+    local m_bIsValveDS = GetProp(CCSGameRulesProxy, "m_bIsValveDS")
+    
+    if m_bIsValveDS == 1 and ui.get(checkbox) == true then
+        SetUi(aimbot, false)
+        SetUi(spread, false)
+        SetUi(recoil, false)
+        SetUi(pitch, "Off")
+        SetUi(yaw, "Off")
+        SetUi(fakeyaw, "Off")
+        SetUi(anti_ut, true)
+    end
+end)
