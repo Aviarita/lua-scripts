1ac949ee9e015d49416e440d715b6ac8e6852e17
Aviarita <43707639+Aviarita@users.noreply.github.com>
2018-09-30 13:11:44 +0200

    Update third_person_dead_key.lua

--- third_person_dead_key/third_person_dead_key.lua
+++ third_person_dead_key/third_person_dead_key.lua
@@ -1,3 +1,3 @@
 local cb_ref = ui.reference("visuals", "effects", "Force third person (dead)")
-local key = ui.new_hotkey("visuals", "effects", "Force third person (dead) ")
-client.set_event_callback("paint", function(ctx) ui.set(cb_ref, ui.get(key)) end)
+local _, key_ref = ui.reference("visuals", "effects", "Force third person (alive)")
+client.set_event_callback("paint", function(ctx) ui.set(cb_ref, ui.get(key_ref)) end)
