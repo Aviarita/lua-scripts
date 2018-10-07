12b7018b6781658eff6f6d8d68a35257d39f4bd6
Aviarita <43707639+Aviarita@users.noreply.github.com>
2018-10-07 02:19:13 +0200

    Update disable_rendering.lua

--- disable_rendering/disable_rendering.lua
+++ disable_rendering/disable_rendering.lua
@@ -2,10 +2,11 @@ local activation = ui.new_checkbox("visuals", "effects", "Disable rendering of e
 client.set_event_callback("paint", function()
 	local players = entity.get_players(false)
 	for i=1, #players do
-		if entity.get_prop(players[i], "m_iTeamNum") ~= entity.get_prop(entity.get_local_player(), "m_iTeamNum") and ui.get(activation) then
-			entity.set_prop(players[i], "m_nRenderMode", 6)
+		local player = players[i]
+		if entity.get_prop(player, "m_iTeamNum") ~= entity.get_prop(entity.get_local_player(), "m_iTeamNum") and ui.get(activation) then
+			entity.set_prop(player, "m_nRenderMode", 6)
 		elseif not ui.get(activation) then
-			entity.set_prop(players[i], "m_nRenderMode", 0)
+			entity.set_prop(player, "m_nRenderMode", 0)
 		end
 	end
 end)
