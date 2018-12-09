595eb2e15d346a689edbc89de00a0ea10761db35
Aviarita <43707639+Aviarita@users.noreply.github.com>
2018-12-09 02:22:18 +0100

    Update slidewalk.lua

--- slidewalk/slidewalk.lua
+++ slidewalk/slidewalk.lua
@@ -1,65 +1,9 @@
-local ffi = require "ffi"
-ffi.cdef[[
-typedef struct {
-    float x;
-    float y; 
-} Vector2;
-typedef struct {
-    int     command_number;     // 0x04 For matching server and client commands for debugging
-    Vector2   viewangles;
-    float    forwardmove;
-    float    sidemove;
-    union {
-        unsigned int buttons;
-        struct {
-            unsigned int in_attack : 1;
-            unsigned int in_jump : 1;
-            unsigned int in_duck : 1;
-            unsigned int in_forward : 1;
-            unsigned int in_back : 1;
-            unsigned int in_use : 1;
-            unsigned int in_cancel : 1;
-            unsigned int in_left : 1;
-            unsigned int in_right : 1;
-            unsigned int in_moveleft : 1;
-            unsigned int in_moveright : 1;
-            unsigned int in_attack2 : 1;
-            unsigned int in_run : 1;
-            unsigned int in_reload : 1;
-            unsigned int in_alt1 : 1;
-            unsigned int in_alt2 : 1;
-            unsigned int in_score : 1;
-            unsigned int in_speed : 1;
-            unsigned int in_walk : 1;
-            unsigned int in_zoom : 1;
-            unsigned int in_weapon1 : 1;
-            unsigned int in_weapon2 : 1;
-            unsigned int in_bullrush : 1;
-            unsigned int in_grenade1 : 1;
-            unsigned int in_grenade2 : 1;
-            unsigned int in_attack3 : 1;
-            unsigned int in_unused : 6;
-        };
-    };
-	int     weaponselect;      
-	int     weaponsubtype;     
-	int     random_seed;       
-	short   mousedx;           
-	short   mousedy;           
-	bool    hasbeenpredicted;  
-} CUserCmd;
-]]
-local ffi_cast = ffi.cast
-local ct_usercmd = ffi.typeof('CUserCmd*')
-
 local slidewalk = ui.new_checkbox("misc", "miscellaneous", "Slidewalk")
-
-client.set_event_callback('setup_command', function(ctx)
-    local cmd = ffi_cast(ct_usercmd, ctx)
+client.set_event_callback('setup_command', function(cmd)
 
     local move_type = entity.get_prop(entity.get_local_player(), "m_MoveType")
 
-    if move_type == 9 then return end -- disable when on ladder
+    if move_type == 9 then return end -- disable when on a ladder
     if cmd.in_use == 1 then return end -- disable when holding e
 
     if ui.get(slidewalk) then
