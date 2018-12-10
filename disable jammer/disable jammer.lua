e2a62dc3a2b06b80541a6e41cdd25d5616816e71
Aviarita <43707639+Aviarita@users.noreply.github.com>
2018-12-10 09:12:53 +0100

    Create disable jammer.lua

---
+++ disable jammer/disable jammer.lua
@@ -0,0 +1,12 @@
+local activated = ui.new_checkbox("visuals", "effects", "Disabled tablet jammer")
+local getprop, setprop, getall, me = entity.get_prop, entity.set_prop, entity.get_all, entity.get_local_player
+client.set_event_callback("run_command", function()
+    local tablets = getall("CTablet")
+    for i=1, #tablets do 
+        local tablet = tablets[i]
+        local m_hOwner = getprop(tablet, "m_hOwner")
+        if m_hOwner == me() then
+            setprop(tablet, "m_bTabletReceptionIsBlocked", not ui.get(activated))
+        end
+    end
+end)
