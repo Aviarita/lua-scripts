2a243c75966f0d42b9353a1412a06af941bb66a4
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-03-28 17:06:25 +0100

    Update increase_hitchance.lua

--- hitchance/increase_hitchance.lua
+++ hitchance/increase_hitchance.lua
@@ -1,20 +1,18 @@
-local GetUi = ui.get
-local SetUi = ui.set
-local SetVisible = ui.set_visible
+local get = ui.get
+local set = ui.set
+local ref, cb, sld = ui.reference, ui.new_checkbox, ui.new_slider
+local setvis = ui.set_visible
+local cllbck = ui.set_callback
 
-local Log = client.log
-local cmd = client.exec
 local AddEvent = client.set_event_callback
 
-local hitchance_ref = ui.reference("RAGE", "Aimbot", "Minimum hit chance")
-local increase_hit_chance_on_miss = ui.new_checkbox("RAGE", "Other", "Increase hit chance on miss")
-local max_missed_shots = ui.new_slider("RAGE", "Other", "Max misses before increasing", 1, 5, 0)
-local increase_hit_chance = ui.new_slider("RAGE", "Other", "Increase hit chance by", 1, 15, 0, true, "%")
-local old_hitchance = ui.new_slider("RAGE", "Other", "Old hit chance", 0, 100, 0, true, "%")
+local hitchance_ref = ref("rage", "aimbot", "minimum hit chance")
+local increase_hit_chance_on_miss = cb("RAGE", "Other", "Increase hit chance on miss")
+local max_missed_shots = sld("RAGE", "Other", "Max misses before increasing", 1, 5, 1)
+local increase_hit_chance = sld("RAGE", "Other", "Increase hit chance by", 1, 15, 1, true, "%")
+local logs = cb("rage", "other", "Enable logging")
 
-SetUi(old_hitchance, GetUi(hitchance_ref))
-
-local missed_shots = 0
+local missed_shots, old_hitchance, new_hitchance = 0, get(hitchance_ref), 1
 
 local function clamp(min, max, current)
 	if current > max then current = max 
@@ -23,30 +21,42 @@ local function clamp(min, max, current)
 end
 
 local function on_aim_miss(event)
-	if not GetUi(increase_hit_chance_on_miss) then return end
-	local current_hitchance = GetUi(hitchance_ref)
+	if not get(increase_hit_chance_on_miss) then return end
+	new_hitchance = get(hitchance_ref) + get(increase_hit_chance)
 	missed_shots = missed_shots + 1
-	if missed_shots >= GetUi(max_missed_shots) and GetUi(hitchance_ref) ~= 100 then
-		Log("Set hit chance to " .. clamp(0, 100, current_hitchance + GetUi(increase_hit_chance)))
-		SetUi(hitchance_ref, clamp(0, 100, current_hitchance + GetUi(increase_hit_chance)))
+	if missed_shots >= get(max_missed_shots) and get(hitchance_ref) ~= 100 then
+		if get(logs) then
+			print("Set hit chance to " .. clamp(0, 100, new_hitchance))
+		end
+		set(hitchance_ref, clamp(0, 100, new_hitchance))
 	end
 end
 
 local function on_aim_hit(event)
-	if not GetUi(increase_hit_chance_on_miss) then return end
+	if not get(increase_hit_chance_on_miss) then return end
 	missed_shots = 0
-	if GetUi(hitchance_ref) ~= GetUi(old_hitchance) then
-		Log("Set hit chance to " .. GetUi(old_hitchance))
-		SetUi(hitchance_ref, GetUi(old_hitchance))
+	if get(hitchance_ref) ~= old_hitchance then
+		if get(logs) then
+			print("Set hit chance to " .. old_hitchance)
+		end
+		set(hitchance_ref, old_hitchance)
 	end
 end
 
-local function change_visibility(event)
-	SetVisible(max_missed_shots, GetUi(increase_hit_chance_on_miss))
-	SetVisible(increase_hit_chance, GetUi(increase_hit_chance_on_miss))
-	SetVisible(old_hitchance, GetUi(increase_hit_chance_on_miss))
-end
-
-AddEvent("run_command", change_visibility)
 AddEvent("aim_miss", on_aim_miss)
 AddEvent("aim_hit", on_aim_hit)
+
+setvis(max_missed_shots, false)
+setvis(increase_hit_chance, false)
+setvis(logs, false)
+cllbck(increase_hit_chance_on_miss, function()
+	setvis(max_missed_shots, get(increase_hit_chance_on_miss))
+	setvis(increase_hit_chance, get(increase_hit_chance_on_miss))
+	setvis(logs, get(increase_hit_chance_on_miss))
+end)
+
+client.set_event_callback("paint", function(ctx) 
+	if get(hitchance_ref) ~= old_hitchance and get(hitchance_ref) ~= clamp(0, 100, new_hitchance) then 
+		old_hitchance = get(hitchance_ref)
+	end
+end)
