1d348f65efd230765b45448474226ca64668fbfe
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-03-31 19:13:24 +0200

    Update ragdoll launcher.lua

--- ragdoll_launcher/ragdoll launcher.lua
+++ ragdoll_launcher/ragdoll launcher.lua
@@ -1,26 +1,19 @@
-local enabled = ui.new_checkbox("misc", "miscellaneous", "Ragdoll launcher")
-local min = ui.new_slider("misc", "miscellaneous", "Radgoll launcher min", -100, 100, 0)
-local max = ui.new_slider("misc", "miscellaneous", "Radgoll launcher max", -100, 100, 0)
-client.set_event_callback("net_update_end", function()
-    if ui.get(enabled) == false then return end
-    local ragdolls = entity.get_all("CCSRagdoll")
+local get, set_prop, get_prop, cb, slider, get_all, add_event = ui.get, entity.set_prop, entity.get_prop, ui.new_checkbox, ui.new_slider, entity.get_all, client.set_event_callback
+local m_enable = cb("misc", "miscellaneous", "Ragdoll launcher")
+local m_mult = slider("misc", "miscellaneous", "\n ragdoll_launcher_multiplier", 0, 250, 0)
+local function launch_ragdoll(ragdoll)
+    local mult = get(m_mult)
+    local force_x, force_y, force_z = get_prop(ragdoll, "m_vecForce")
+    local ragdollvelocity_x, ragdollvelocity_y, ragdollvelocity_z = get_prop(ragdoll, "m_vecRagdollVelocity")
+    set_prop(ragdoll, "m_vecForce", force_x * mult, force_y * mult, force_z * mult)
+    set_prop(ragdoll, "m_vecRagdollVelocity", ragdollvelocity_x * mult, ragdollvelocity_y * mult, ragdollvelocity_z * mult)
+end
+add_event("net_update_end", function()
+    if get(m_enable) == false then return end
+    local ragdolls = get_all("CCSRagdoll")
     for i=1, #ragdolls do 
         local ragdoll = ragdolls[i]
         if ragdoll == nil then return end
-
-        local mult = client.random_int(ui.get(min), ui.get(max))
-
-        local force_x, force_y, force_z = entity.get_prop(ragdoll, "m_vecForce")
-        local ragdollvelocity_x, ragdollvelocity_y, ragdollvelocity_z = entity.get_prop(ragdoll, "m_vecRagdollVelocity")
-
-        entity.set_prop(ragdoll, "m_vecForce", force_x * mult, force_y * mult, force_z * mult)
-        entity.set_prop(ragdoll, "m_vecRagdollVelocity", ragdollvelocity_x * mult, ragdollvelocity_y * mult, ragdollvelocity_z * mult)
+        launch_ragdoll(ragdoll)
     end
-end)
-
-ui.set_visible(min, ui.get(enabled))
-ui.set_visible(max, ui.get(enabled))
-ui.set_callback(enabled, function(self)
-    ui.set_visible(min, ui.get(self))
-    ui.set_visible(max, ui.get(self))
 end)
