c755386f299f050ebec9a7241b51f265d6d79297
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-08-02 21:37:57 +0200

    Update legit_aa.lua
    Added "display angles" checkbox, when enabled, it will show you your anti-aim angles in thirdperson. Be careful though as it uses the ragebot to do so.

--- legit_aa/legit_aa.lua
+++ legit_aa/legit_aa.lua
@@ -1,15 +1,35 @@
+local enabled = ui.new_checkbox("aa", "anti-aimbot angles", "Legit AA")
+local swap_sides = ui.new_hotkey("aa", "anti-aimbot angles", "Legit AA", true)
+local disable_when_moving = ui.new_slider("aa", "anti-aimbot angles", "Velocity threshold", 1, 250, 10)
+local display_angles = ui.new_checkbox("aa", "anti-aimbot angles", "Display angles(Ragebot)")
+
 local yaw, yaw_slider = ui.reference("aa", "anti-aimbot angles", "yaw")
 local body_yaw, body_yaw_slider = ui.reference("aa", "anti-aimbot angles", "body yaw")
 local lby_target = ui.reference("aa", "anti-aimbot angles", "lower body yaw target")
 local flag_limit = ui.reference("aa", "fake lag", "Limit")
-local enabled = ui.new_checkbox("aa", "anti-aimbot angles", "Legit AA")
-local swap_sides = ui.new_hotkey("aa", "anti-aimbot angles", "Legit AA", true)
-local disable_when_moving = ui.new_slider("aa", "anti-aimbot angles", "Velocity threshold", 1, 250, 10)
 
-local set, get = ui.set, ui.get
+local ragebot = ui.reference("rage", "aimbot", "enabled")
+local target_hitbox = ui.reference("rage", "aimbot", "target hitbox")
+local minimum_hitchance = ui.reference("rage", "aimbot", "minimum hit chance")
+local minimum_minimum_damage = ui.reference("rage", "aimbot", "minimum damage")
+local fov = ui.reference("rage", "aimbot", "maximum fov")
+local fake_shadow = ui.reference("visuals", "colored models", "local fake shadow")
+
+local set, get, vis = ui.set, ui.get, ui.set_visible
 local floor, min, sqrt = math.floor, math.min, math.sqrt
 local get_prop, get_lp = entity.get_prop, entity.get_local_player
 local indicator = renderer.indicator
+
+local function length3d(self)
+    return floor(min(10000, sqrt( 
+		( self[1] * self[1] ) +
+		( self[2] * self[2] ) +
+		( self[3] * self[3] ))+ 0.5)
+	)
+end
+
+vis(disable_when_moving, false)
+vis(display_angles, false)
 
 ui.set_callback(enabled, function(self)
     if get(self) then 
@@ -26,14 +46,23 @@ ui.set_callback(enabled, function(self)
         set(body_yaw_slider, 0)
         set(lby_target, "Off")
     end
+    vis(disable_when_moving, get(self))
+    vis(display_angles, get(self))
 end)
-local function length3d(self)
-    return floor(min(10000, sqrt( 
-		( self[1] * self[1] ) +
-		( self[2] * self[2] ) +
-		( self[3] * self[3] ))+ 0.5)
-	)
-end
+
+ui.set_callback(display_angles, function(self)
+    if get(enabled) then 
+        local state = get(self)
+        set(ragebot, state)
+        set(fake_shadow, state)
+         if state then 
+            set(target_hitbox, "feet")
+            set(minimum_hitchance, 100)
+            set(minimum_minimum_damage, 126)
+            set(fov, 1)
+        end
+    end
+end)
 
 local disabled_aa = false
 client.set_event_callback("setup_command", function(cmd)
