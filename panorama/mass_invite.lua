ca84baba506a9770089c63cf57031bdba0eb0596
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-09-25 10:43:39 +0200

    Create mass_invite.lua

---
+++ panorama/mass_invite.lua
@@ -0,0 +1,63 @@
+local js = require("access_panorama")
+
+js.eval([[
+    let collectedSteamIDS = [];
+    collectedSteamIDS.push("123");
+]])
+
+local refresh = false
+local function refresh_nearbies()
+    client.delay_call(5, refresh_nearbies)
+    if not refresh then 
+        return
+    end
+    js.eval([[
+        PartyBrowserAPI.Refresh();
+        var lobbies = PartyBrowserAPI.GetResultsCount();
+        for (var lobbyid = 0; lobbyid < lobbies; lobbyid++) {
+            var xuid = PartyBrowserAPI.GetXuidByIndex(lobbyid);
+            if (!collectedSteamIDS.includes(xuid)) {
+                if (collectedSteamIDS.includes('123')) {
+                    collectedSteamIDS.splice(collectedSteamIDS.indexOf('123'), 1);
+                }
+                collectedSteamIDS.push(xuid);
+                $.Msg(`Adding ${xuid} to the collection..`);
+            }
+        }
+        $.Msg(`Mass invite collection: ${collectedSteamIDS.length}`);
+    ]]) 
+end
+refresh_nearbies()
+local auto_refresh_nearbies = ui.new_checkbox("lua", "a", "Auto refresh nearbies")
+ui.set_callback(auto_refresh_nearbies, function(self)
+    refresh = ui.get(self)
+end)
+ui.new_button("lua", "a", "Refresh nearbies", function()
+    js.eval([[
+        PartyBrowserAPI.Refresh();
+        var lobbies = PartyBrowserAPI.GetResultsCount();
+        for (var lobbyid = 0; lobbyid < lobbies; lobbyid++) {
+            var xuid = PartyBrowserAPI.GetXuidByIndex(lobbyid);
+            if (!collectedSteamIDS.includes(xuid)) {
+                if (collectedSteamIDS.includes('123')) {
+                    collectedSteamIDS.splice(collectedSteamIDS.indexOf('123'), 1);
+                }
+                collectedSteamIDS.push(xuid);
+                $.Msg(`Adding ${xuid} to the collection..`);
+            }
+        }
+        $.Msg(`Mass invite collection: ${collectedSteamIDS.length}`);
+    ]]) 
+end)
+ui.new_button("lua", "a", "Mass invite nearbies", function()
+    js.eval([[
+        collectedSteamIDS.forEach(xuid => {
+            FriendsListAPI.ActionInviteFriend(xuid, "");
+        });
+    ]])
+end)
+ui.new_button("lua", "a", "Print invite collection", function()
+    js.eval([[
+        $.Msg(collectedSteamIDS);
+    ]])
+end)
