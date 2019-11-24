a4c2d15d93f9e2b5cb1aa5d8eca7da692feeb6c3
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-11-24 16:31:54 +0100

    Update aaaaaaaaaa.lua

--- revofix/aaaaaaaaaa.lua
+++ revofix/aaaaaaaaaa.lua
@@ -1,10 +1,13 @@
 local ui_reference = ui.reference
 local twist = ui.new_checkbox("AA", "Anti-aimbot angles", "Twist(Useless)")
+local twist = ui.new_checkbox("RAGE", "Aimbot", "Stomach hitbox scale(Useless)")
 function ui.reference(tab, container, name)
     if name == "Infinite duck" then 
         return ui_reference("misc", "movement", "Infinite duck")
     elseif name == "Twist" then 
         return twist
+    elseif name == "Stomach hitbox scale" then 
+        return shs        
     end
     return ui_reference(tab, container, name)
 end
