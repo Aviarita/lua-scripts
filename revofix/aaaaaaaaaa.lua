9ef7f33f4243e0f90d8c16bad7b4eed4b9ecafa3
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-11-24 16:48:12 +0100

    Update aaaaaaaaaa.lua

--- revofix/aaaaaaaaaa.lua
+++ revofix/aaaaaaaaaa.lua
@@ -1,13 +1,17 @@
 local ui_reference = ui.reference
 local twist = ui.new_checkbox("AA", "Anti-aimbot angles", "Twist(Useless)")
 local shs = ui.new_checkbox("RAGE", "Aimbot", "Stomach hitbox scale(Useless)")
+local psp = ui_reference("RAGE", "Aimbot", "Prefer safe point")
+local fsp = ui_reference("RAGE", "Aimbot", "Force safe point")
 function ui.reference(tab, container, name)
     if name == "Infinite duck" then 
         return ui_reference("misc", "movement", "Infinite duck")
     elseif name == "Twist" then 
         return twist
     elseif name == "Stomach hitbox scale" then 
-        return shs        
+        return shs      
+    elseif name == "Safe point" then 
+        return psp, fsp   
     end
     return ui_reference(tab, container, name)
 end
