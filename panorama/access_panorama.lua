c7d930302026fa81ecd232cd5217da25a662f229
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-12-12 18:41:46 +0100

    Update access_panorama.lua

--- panorama/access_panorama.lua
+++ panorama/access_panorama.lua
@@ -19,36 +19,51 @@ local panoramaengine_vtbl = panoramaengine[0] -- void**
 local access_ui_engine = ffi.cast("access_ui_engine_t", panoramaengine_vtbl[11]) -- void*
 
 local function get_last_target_panel(uiengineptr)
-    local a_get_last_target_panel = ffi.cast("get_last_target_panel_t", uiengineptr[0][56])
-    return a_get_last_target_panel(uiengineptr)
+    local vtbl = uiengineptr[0] or error("uiengineptr is nil", 2)
+    local func = vtbl[56] or error("uiengineptr_vtbl is nil", 2)
+    local fn = ffi.cast("get_last_target_panel_t", func)
+    return fn(uiengineptr)
 end
 
 local function is_valid_panel_ptr(uiengineptr, itr)
-    local a_is_valid_panel_ptr = ffi.cast("is_valid_panel_ptr_t", uiengineptr[0][36])
-    return a_is_valid_panel_ptr(uiengineptr, itr)
+    if itr == nil then 
+        return false --error("itr is nil", 2)
+    end
+    local vtbl = uiengineptr[0] or error("uiengineptr is nil", 2)
+    local func = vtbl[36] or error("uiengineptr_vtbl is nil", 2)
+    local fn = ffi.cast("is_valid_panel_ptr_t", func)
+    return fn(uiengineptr, itr)
 end
 
 local function get_panel_id(panelptr)
-    local a_get_panel_id = ffi.cast("get_panel_id_t", panelptr[0][9])
-    return ffi.string(a_get_panel_id(panelptr))
+    local vtbl = panelptr[0] or error("panelptr is nil", 2)
+    local func = vtbl[9] or error("panelptr_vtbl is nil", 2)
+    local fn = ffi.cast("get_panel_id_t", func)
+    return ffi.string(fn(panelptr))
 end
 
 local function set_visible(panelptr, state)
-    local a_set_visible = ffi.cast("set_visible_t", panelptr[0][27])
-    a_set_visible(panelptr, state)
+    local vtbl = panelptr[0] or error("panelptr is nil", 2)
+    local func = vtbl[27] or error("panelptr_vtbl is nil", 2)
+    local fn = ffi.cast("set_visible_t", func)
+    fn(panelptr, state)
 end
 
 local function get_parent(panelptr)
-    local a_get_parent = ffi.cast("get_parent_t", panelptr[0][25])
-    return a_get_parent(panelptr)
+    local vtbl = panelptr[0] or error("panelptr is nil", 2)
+    local func = vtbl[25] or error("panelptr_vtbl is nil", 2)
+    local fn = ffi.cast("get_parent_t", func)
+    return fn(panelptr)
 end
 
 local function get_root(uiengineptr, custompanel)
-    local panel = get_last_target_panel(uiengineptr)
-    local itr = panel
+    local itr = get_last_target_panel(uiengineptr) 
+    if itr == nil then 
+        return
+    end
     local ret = nil
-    local panelptr = ffi.cast("void***", itr)
-    while itr and is_valid_panel_ptr(uiengineptr, itr) do 
+    local panelptr = nil
+    while itr ~= nil and is_valid_panel_ptr(uiengineptr, itr) do 
         panelptr = ffi.cast("void***", itr)
         if custompanel and get_panel_id(panelptr) == custompanel then 
             ret = itr
@@ -60,7 +75,7 @@ local function get_root(uiengineptr, custompanel)
             ret = itr
             break
         end
-        itr = get_parent(panelptr)
+        itr = get_parent(panelptr) or error("Couldn't get parent..", 2)
     end
     return ret
 end
@@ -68,20 +83,21 @@ end
 local uiengine = ffi.cast("void***", access_ui_engine(panoramaengine))
 local run_script = ffi.cast("run_script_t", uiengine[0][113])
 
-local rootpanel = get_root(uiengine)
+local rootpanel = get_root(uiengine) or error("Couldn't get root panel..", 2)
 
-local function eval(code, custompanel)
+local function eval(code, custompanel, customFile)
     if custompanel then 
         rootpanel = custompanel
     else
         if rootpanel == nil then    
-            rootpanel = get_root(uiengine)
+            rootpanel = get_root(uiengine) or error("Couldn't get root panel..", 2)
         end
     end
-    run_script(uiengine, rootpanel, ffi.string(code), "panorama/layout/base_mainmenu.xml", 8, 10, false, false)
+    local file = customFile or "panorama/layout/base_mainmenu.xml"
+    run_script(uiengine, rootpanel, ffi.string(code), file, 8, 10, false, false)
 end
 local function get_child(childname)
-    return get_root(uiengine, childname)
+    return get_root(uiengine, childname) or error("Couldn't get root panel..", 2)
 end
 local function change_visiblity(childptr, state)
     local panelptr = ffi.cast("void***", childptr)
