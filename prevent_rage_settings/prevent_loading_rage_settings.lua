db760995e1feb535dee396d83b888337049eb356
Aviarita <43707639+Aviarita@users.noreply.github.com>
2019-12-22 12:26:28 +0100

    Update prevent_loading_rage_settings.lua

--- prevent_rage_settings/prevent_loading_rage_settings.lua
+++ prevent_rage_settings/prevent_loading_rage_settings.lua
@@ -3,31 +3,21 @@ local GetUi = ui.get
 local aimbot = ui.reference("RAGE", "Aimbot", "Enabled")
 local spread = ui.reference("RAGE", "Other", "Remove spread")
 local recoil = ui.reference("RAGE", "Other", "Remove recoil")
-local pitch = ui.reference("AA", "Anti-Aimbot angles", "Pitch")
-local yaw = ui.reference("AA", "Anti-Aimbot angles", "Yaw")
-local bodyyaw = ui.reference("AA", "Anti-Aimbot angles", "Body yaw")
-local lby = ui.reference("AA", "Anti-Aimbot angles", "Lower body yaw target")
+local antiaim = ui.reference("AA", "Anti-Aimbot angles", "Enabled")
 local anti_ut = ui.reference("MISC", "Settings", "Anti-untrusted")
 
-local checkbox = ui.new_checkbox("MISC", "Other", "Prevent loading rage settings")
+local checkbox = ui.new_checkbox("MISC", "Settings", "Prevent loading rage settings")
 
 local GetAll = entity.get_all
 local GetProp = entity.get_prop
-
-local AddEvent = client.set_event_callback
-
-AddEvent("run_command", function()
+client.set_event_callback("run_command", function()
     local CCSGameRulesProxy = GetAll("CCSGameRulesProxy")[1]
-    local m_bIsValveDS = GetProp(CCSGameRulesProxy, "m_bIsValveDS")
-    
+    local m_bIsValveDS = GetProp(CCSGameRulesProxy, "m_bIsValveDS")    
     if m_bIsValveDS == 1 and ui.get(checkbox) == true then
         SetUi(aimbot, false)
         SetUi(spread, false)
         SetUi(recoil, false)
-        SetUi(pitch, "Off")
-        SetUi(yaw, "Off")
-        SetUi(lby, "Off")
-        SetUi(bodyyaw, "Off")
+        SetUi(antiaim, false)
         SetUi(anti_ut, true)
     end
 end)
